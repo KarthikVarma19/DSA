018d500ad0ecd116d9024c5f527e97f9cdefe4cf
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2024-11-26 19:41:49 +0530

    Time: 0 ms (100%), Space: 45.7 MB (11.66%) - LeetHub

--- 3188-find-champion-i/3188-find-champion-i.java
+++ 3188-find-champion-i/3188-find-champion-i.java
@@ -1,19 +1,9 @@
 class Solution {
     public int findChampion(int[][] grid) {
-        int weaker[] = new int[grid.length];
-        int count = 0;
-        int champion = -1;
+        int candidate = 0;
         for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid.length;j++){
-                if(grid[i][j] == 0 && i!=j){
-                    weaker[i]++;
-                }
-            }
-            if(weaker[i] == 0){
-                count++;
-                champion = i;
-            }
+            if(grid[i][candidate] == 1) candidate = i;
         }
-        return count==1?champion:-1;
+        return candidate;
     }
 }
