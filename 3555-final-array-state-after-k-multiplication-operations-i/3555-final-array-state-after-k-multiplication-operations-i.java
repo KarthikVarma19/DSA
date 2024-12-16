4a2cb06e936d15b4d0f2ac3cc8a3befd2ceeb7e6
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2024-12-16 06:45:09 +0530

    Time: 1 ms (100%), Space: 45.1 MB (20.61%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
@@ -1,31 +1,19 @@
 class Solution {
-    class pair{
-        int value;
-        int index;
-        pair(int value,int index){
-            this.value = value;
-            this.index = index;
-        }
-    }
     public int[] getFinalState(int[] nums, int k, int multiplier) {
-        PriorityQueue<pair> pq = new PriorityQueue<>(new Comparator<pair>(){
-            @Override 
-            public int compare(pair a, pair b){
-                if(a.value == b.value){
-                    return a.index-b.index;
-                }
-                return a.value-b.value;
-            }
-        });
-        for(int i=0;i<nums.length;i++){
-            pq.add(new pair(nums[i],i));
-        }
         while(k-- > 0){
-            int topValue = pq.peek().value, topIndex = pq.peek().index;
-            pq.remove();
-            nums[topIndex] = topValue * multiplier;
-            pq.add(new pair(nums[topIndex],topIndex));
+            int minIndex = getMin(nums);
+            nums[minIndex] *= multiplier;
         }
         return nums;
+    }
+    public int getMin(int nums[]){
+        int minValue = nums[0], minIndex = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] < minValue){
+                minValue = nums[i];
+                minIndex = i;
+            }
+        }
+        return minIndex;
     }
 }
