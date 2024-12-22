f910f3687233123ddfa033d67b16d2c10d08a874
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2024-12-22 16:47:35 +0530

    Time: 87 ms (92.02%), Space: 68 MB (5.6%) - LeetHub

---
+++ 0502-ipo/0502-ipo.java
@@ -0,0 +1,29 @@
+class Solution {
+    private static class Project{
+        int capital;
+        int profit;
+        Project(int capital, int profit){
+            this.capital = capital;
+            this.profit = profit;
+        }
+    }
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        int n = profits.length;
+        List<Project> projects = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            projects.add(new Project(capital[i],profits[i]));
+        }
+        Collections.sort(projects, (a,b)-> a.capital-b.capital);
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a,b) -> b-a);
+        int i = 0;
+        while(k-->0){
+            while(i<n && projects.get(i).capital <= w){
+                maxHeap.add(projects.get(i).profit);
+                i++;
+            }
+            if(maxHeap.isEmpty()) break;
+            w += maxHeap.remove();
+        }
+        return w;
+    }
+}
