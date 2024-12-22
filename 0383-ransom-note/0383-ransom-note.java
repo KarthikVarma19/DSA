9b177163cd72e4617b580530751c4041b7ed8b9f
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2024-12-22 19:10:05 +0530

    Time: 3 ms (72.22%), Space: 44.9 MB (64.43%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int freq[] = new int[26];
+        for(int i=0;i<magazine.length();i++){
+            char cur = magazine.charAt(i);
+            freq[cur-'a']++;
+        }
+        for(int i=0;i<ransomNote.length();i++){
+            char cur = ransomNote.charAt(i);
+            freq[cur-'a']--;
+            if(freq[cur-'a'] < 0) return false;
+        }
+        return true;
+    }
+}
