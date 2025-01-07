7bcc3a9459e714607f4e8e8e601667b8de1a0f44
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-01-07 07:19:39 +0530

    Time: 4 ms (86.21%), Space: 45.6 MB (6%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
@@ -1,13 +1,61 @@
+class Node{
+    int count;
+    Node links[];
+    Node(){
+        count = 0;
+        links = new Node[26];
+    }
+    boolean contains(char ch){
+        return links[ch-'a'] != null;
+    }
+    void createNode(char ch){
+        links[ch-'a'] = new Node();
+    }
+    void increment(){
+        this.count++;
+    }
+    Node get(char ch){
+        return links[ch-'a'];
+    }
+    int getCount(){
+        return count;
+    }
+}
+class Trie{
+    Node root;
+    Trie(){
+        root = new Node();
+    }
+    void insert(String word){
+        Node node = root;
+        for(char ch: word.toCharArray()){
+            if(!node.contains(ch)){
+                node.createNode(ch);
+            }
+            node = node.get(ch);
+            node.increment();
+        }
+    }
+    boolean isSubstring(String word){
+        Node node = root;
+        for(char ch: word.toCharArray()){
+            node = node.get(ch);
+        }
+        return node.getCount() > 1;
+    }
+}
 class Solution {
     public List<String> stringMatching(String[] words) {
+        Trie root = new Trie();
+        for(String each: words){
+            for(int i=0;i<each.length();i++){
+                root.insert(each.substring(i));
+            }
+        }
         List<String> ans = new ArrayList<>();
-        for(int i = 0; i < words.length; i++){
-            String word = words[i];
-            for(int j = 0; j < words.length; j++){
-                if(i == j) continue;
-                if( word.contains(words[j]) && !ans.contains(words[j])){
-                    ans.add(words[j]);
-                }
+        for(String each: words){
+            if(root.isSubstring(each)){
+                ans.add(each);
             }
         }
         return ans;
