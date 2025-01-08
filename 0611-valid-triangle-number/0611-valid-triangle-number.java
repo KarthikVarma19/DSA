55601b380dd02913134ed845cc87776482af2901
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-01-08 11:03:17 +0530

    Time: 1477 ms (5.13%), Space: 43.4 MB (43.2%) - LeetHub

---
+++ 0611-valid-triangle-number/0611-valid-triangle-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    private boolean isValidTriangle(int a, int b, int c){
+        if(((a+b)>c) && ((b+c)>a) && (c+a)>b){
+            return true;
+        } 
+        return false;
+    }
+    public int triangleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int count = 0;
+        int len = nums.length;
+        for(int i = 0; i < len; i++){
+            for(int j = i + 1; j < len; j++){
+                for(int k = j + 1; k < len; k++){
+                    if(isValidTriangle(nums[i],nums[j],nums[k]) == true){
+                        count++;
+                    }
+                    else{
+                        break;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
