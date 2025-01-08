032d2b7b163647c8bcd639808923a572233b1545
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-01-08 10:17:57 +0530

    Time: 92 ms (50%), Space: 213.3 MB (30.55%) - LeetHub

---
+++ 3305-count-prefix-and-suffix-pairs-ii/3305-count-prefix-and-suffix-pairs-ii.java
@@ -0,0 +1,28 @@
+class TrieNode{
+    HashMap<Integer,TrieNode> links;
+    int count;
+    TrieNode(){
+        links = new HashMap<>();
+        count = 0;
+    }
+}
+class Solution {
+    private static int hash = 128;
+    public long countPrefixSuffixPairs(String[] words) {
+        long count = 0;
+        TrieNode root = new TrieNode();
+        for(String word: words){
+            TrieNode node = root;
+            for(int i = 0, n = word.length(); i < n; i++){
+                int key = word.charAt(i) << hash + word.charAt(n-i-1);
+                if(!node.links.containsKey(key)){
+                    node.links.put(key, new TrieNode());
+                }
+                node = node.links.get(key);
+                count += node.count;
+            }
+            node.count++;
+        }
+        return count;
+    }
+}
