8da19a4994a1eb55da5928b9ce89bf8c00143b02
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-01-09 06:15:03 +0530

    Time: 9 ms (6.21%), Space: 44.1 MB (5.41%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.java
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.java
@@ -1,8 +1,45 @@
+class TrieNode{
+    TrieNode links[];
+    boolean isEnd;
+
+    TrieNode(){
+        links = new TrieNode[26];
+        isEnd = false;
+    }
+
+    void insert(String prefix, TrieNode root){
+        TrieNode node = root;
+        for(char ch: prefix.toCharArray()){
+            if(node.links[ch-'a'] == null){
+                node.links[ch-'a'] = new TrieNode();
+            }
+            node = node.links[ch-'a'];
+        }
+        node.isEnd = true;
+    }
+
+    boolean matches(String word, TrieNode root){
+        TrieNode node = root;
+        for(char ch: word.toCharArray()){
+            if(node.links[ch-'a'] == null){
+                return false;
+            }
+            node = node.links[ch-'a'];
+            if(node.isEnd == true){
+                return true;
+            }
+        }
+        return node.isEnd;
+    }
+}
 class Solution {
     public int prefixCount(String[] words, String pref) {
+        TrieNode root = new TrieNode();
+        root.insert(pref, root);
         int count = 0;
-        for(String word: words){
-            if(word.startsWith(pref)){
+        for(String each: words){
+            if(root.matches(each,root)){
+                System.out.println(each);
                 count += 1;
             }
         }
