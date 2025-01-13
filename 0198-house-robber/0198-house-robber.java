e647977b5c225d6afb38d1d6d30daf5f8aef10e9
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-01-13 05:42:31 +0530

    Time: 0 ms (100%), Space: 41.6 MB (7.2%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -19,6 +19,23 @@ class Solution {
         int n = nums.length;
         int dp[] = new int[n];
         Arrays.fill(dp, -1);
-        return f(n-1, nums, dp);
+        //return f(n-1, nums, dp);
+        return t(nums, dp);
+    }
+
+    public int t(int[] nums, int dp[]){
+        int n = nums.length;
+        dp[0] = nums[0];
+        int neg = 0;
+        for(int i=1;i<n;i++){
+            int pick = nums[i];
+            if(i>1){
+                pick += dp[i-2];
+            }
+            int notpick = dp[i-1];
+
+            dp[i] = Math.max(pick, notpick);
+        }
+        return dp[n-1];
     }
 }
