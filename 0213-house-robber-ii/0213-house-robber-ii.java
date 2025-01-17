310766553e50c1323e63ecfaf7c3ab18cadb315d
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-01-17 21:33:50 +0530

    Time: 0 ms (100%), Space: 41.3 MB (26.18%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,5 +1,41 @@
 class Solution {
-    public int rob(int[] nums) {
+public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 1){
+            return nums[0];
+        }
+        int nums1[] = new int[n-1];
+        int nums2[] = new int[n-1];
+        for(int i = 0; i < n; i++){
+            if(i!=0){
+                nums1[i-1] = nums[i];
+            }
+            if(i!=n-1){
+                nums2[i] = nums[i];
+            }
+        }
+        int dp[] = new int[n];
+        Arrays.fill(dp, -1);
+        int rob1 = f(n-2,nums1, dp);
+        Arrays.fill(dp, -1);
+        int rob2 = f(n-2,nums2, dp);
+        return Math.max(rob1, rob2);
+    }
+    public int f(int n, int a[], int dp[]){
+        dp[0] = a[0];
+        for(int ind = 1; ind <= n; ind++){
+            int take = a[ind];
+            if(ind > 1){
+                take += dp[ind-2];
+            } 
+            int notTake = 0 + dp[ind-1];
+            dp[ind] = Math.max(take, notTake);
+        }
+        return dp[n];
+    }
+
+    //Memoization Code
+    /*public int rob(int[] nums) {
         int n = nums.length;
         if(n == 1){
             return nums[0];
@@ -35,6 +71,10 @@ class Solution {
         int notTake = 0 + f(ind-1, a, dp);
         return dp[ind] = Math.max(take, notTake);
     }
+    */
+
+    //Recursive Code
+
     /*
     public int rob(int[] nums) {
         int n = nums.length;
