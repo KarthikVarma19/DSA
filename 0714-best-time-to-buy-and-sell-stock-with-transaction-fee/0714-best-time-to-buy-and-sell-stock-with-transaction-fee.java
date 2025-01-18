e0160946b34ed43f862c1374d9eda02e61de1a98
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-01-18 19:59:08 +0530

    Time: 16 ms (48.33%), Space: 55.2 MB (68.43%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,4 +1,38 @@
 class Solution {
+    //Tabulation Code
+    int fee;
+    public int maxProfit(int[] prices, int fee) {
+        this.fee = fee;
+        return f(prices);
+    }
+    //Two Changing Parameters
+    public int f(int prices[]){
+        int n = prices.length;
+        int dp[][] = new int[n+1][2];
+        dp[n][0] = 0;
+        dp[n][1] = 0;
+        //ind n-1 to 0
+        //buy o to 1
+
+        for(int ind = n-1; ind >= 0; ind--){
+            for(int buy = 0; buy < 2; buy++){
+                int profit = 0;
+                if(buy == 1){
+                    profit = Math.max((-prices[ind] + dp[ind+1][0])
+                            , (dp[ind+1][1]));
+                }
+                else{
+                    profit = Math.max((-fee + prices[ind] + dp[ind+1][1])
+                    , (dp[ind+1][0]));
+                }
+                dp[ind][buy] = profit;
+            }
+        }
+        return dp[0][1];
+    } 
+
+
+    /*
     //Memoization Code
     // Beats 27%
     int fee;
@@ -27,6 +61,30 @@ class Solution {
             profit = Math.max((-fee + prices[ind] + f(ind+1, 1, prices, dp)), (0 + f(ind+1, 0, prices, dp)));
         }
         return dp[ind][buy] = profit;
+    } 
+    */
+    
+    /*
+    //Recursion Code
+    //Time Limit Exceeded 19/44 testcases passed
+    int fee;
+    public int maxProfit(int[] prices, int fee) {
+        this.fee = fee;
+        return f(0,1,prices);
     }
+    public int f(int ind, int buy, int prices[]){
+        if(ind == prices.length){
+            return 0;
+        }
+        int profit = 0;
+        if(buy == 1){
+            profit = Math.max((-prices[ind] + f(ind+1, 0, prices)), (f(ind+1, 1, prices)));
+        }
+        else{
+            profit = Math.max((-fee + prices[ind] + f(ind+1, 1, prices)), (0 + f(ind+1, 0, prices)));
+        }
+        return profit;
+    }
+    */
    
 }
