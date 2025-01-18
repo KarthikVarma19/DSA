e05b2a776084219168f4f712b06b3485e4ade074
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-01-18 16:26:52 +0530

    Time: 2 ms (79.18%), Space: 44.6 MB (41.9%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,5 +1,49 @@
 class Solution {
+    //Tabulation Code 
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        int dp[][][] = new int[n+1][3][k+1];
+        return f(k, prices, dp);
+    }
+    //Bottom-Up Approach
+    public int f(int k, int prices[], int dp[][][]){
+        int n = prices.length;
+        //No Problem to Declare Because Initially dp consistis of zero's 
+        //Declare the Base Case 
+        for(int buy = 0; buy < 3; buy++){
+            for(int ki = 0; ki < k; ki++){
+                dp[n][buy][ki] = 0;
+            }
+        }
+
+        //ind n-1 to 0 
+        //buy 0 to 1 
+        //k should be 0 to k
+        for(int ind = n-1; ind >= 0; ind--){
+            for(int buy = 0; buy < 2; buy++){
+                for(int ki = 1; ki <= k; ki++){
+                    int profit = 0;
+                    if(buy == 1){
+                        if(ki > 0){
+                            profit = Math.max((-prices[ind] + dp[ind+1][0][ki]),
+                                             (dp[ind+1][1][ki]));
+                        }
+                    }
+                    else{
+                        profit = Math.max((prices[ind] + dp[ind+1][1][ki-1]), 
+                        (dp[ind+1][0][ki]));
+                    }
+
+                    dp[ind][buy][ki] = profit;
+                }
+            }
+        }
+        
+        return dp[0][1][k];
+    }
+    /*
     //Memoization Code
+    //Beats 32%
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
         int dp[][][] = new int[n][3][k+1];
@@ -10,7 +54,7 @@ class Solution {
         }
         return f(0, 1, k, prices, dp);
     }
-    //Three Changing Parameters ind, buy, k
+    //Three Changing Parameters ind, buy, k so 3D DP Array is Required
     public int f(int ind, int buy,  int k, int prices[], int dp[][][]){
         if(ind == prices.length || k == 0){
             return 0;
@@ -30,6 +74,11 @@ class Solution {
 
         return dp[ind][buy][k] = profit;
     }
+    */
+
+
+
+
 
     /*
     //Recursion Code
