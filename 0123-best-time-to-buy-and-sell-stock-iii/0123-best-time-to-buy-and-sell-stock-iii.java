a8a84f008cb3309cf9c3788839006886870beadb
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-01-18 12:19:55 +0530

    Time: 8 ms (49.07%), Space: 60.9 MB (83.96%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,6 +1,44 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
+
+        return f(prices);
+    }
+    public int f(int[] prices){
+        int ahead[][] = new int[2][3];
+        int curi[][] = new int[2][3];
+        int n = prices.length;
+
+        for(int ind = n-1; ind>=0; ind--){
+            for(int buy = 0; buy < 2; buy++){
+                for(int times = 1; times <=2; times++){
+                    int profit = 0;
+                    if(buy == 1){
+                        if(times > 0){
+                            profit = Math.max((-prices[ind] + ahead[0][times])
+                            ,(0 + ahead[1][times]));
+                        }  
+                    }
+                    else{
+                        profit = Math.max((prices[ind] + ahead[1][times-1])
+                                    , (0 + ahead[0][times]));
+                    }
+                    curi[buy][times] = profit;
+                }
+            }
+            ahead = curi;        
+        }
+        return ahead[1][2];
+    }
+    
+    
+    
+    
+    /* Tabulation Code
+    
+    //Remember Remember You Should Decrement the times only after selling the stock which means after one transaction
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
         int dp[][][] = new int[n+1][2][3];
 
         return f(prices, dp);
@@ -42,6 +80,12 @@ class Solution {
         return dp[0][1][2];
     }
     
+    */
+
+
+
+
+
     /* Memoization Code
     public int maxProfit(int[] prices) {
         int n = prices.length;
