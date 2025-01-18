ba89e8862e352d33c806c81c59ac81ee5879bc68
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-01-18 11:08:58 +0530

    Time: 1 ms (88.98%), Space: 46.2 MB (22.92%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,4 +1,37 @@
 class Solution {
+     public int maxProfit(int[] prices) {
+        return f(prices);
+    }
+    public int f(int prices[]){
+        int n = prices.length;
+        int prev[] = new int[2];
+        int curi[] = new int[2];
+        prev[0] = 0;
+        prev[1] = 0;
+
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                int profit = 0;
+                if(buy == 1){
+                    profit = Math.max((-prices[ind] + prev[0])
+                                                ,(0 + prev[1]));
+                }
+                else{
+                    profit = Math.max((prices[ind] + prev[1]), 
+                                    (0 + prev[0]));
+                }
+                curi[buy] = profit;
+            }
+            prev = curi;
+        }
+
+        return prev[1];
+    }
+
+
+
+
+    /*
     public int maxProfit(int[] prices) {
         int n = prices.length;
         int dp[][] = new int[n+1][2];
@@ -26,7 +59,13 @@ class Solution {
 
         return dp[0][1];
     }
+    */
+
+
+
+
     /*
+    Memoization Code
     public int maxProfit(int[] prices) {
         int n = prices.length;
         int dp[][] = new int[n][2];
