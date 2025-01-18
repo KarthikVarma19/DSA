57364ae0d2e77b8cd8977a0b16789b3554a351b0
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-01-18 11:12:25 +0530

    Time: 0 ms (100%), Space: 45.9 MB (54.34%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,5 +1,5 @@
 class Solution {
-     public int maxProfit(int[] prices) {
+    public int maxProfit(int[] prices) {
         return f(prices);
     }
     public int f(int prices[]){
@@ -27,70 +27,4 @@ class Solution {
 
         return prev[1];
     }
-
-
-
-
-    /*
-    public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int dp[][] = new int[n+1][2];
-        return f(prices, dp);
-    }
-    public int f(int prices[], int dp[][]){
-        int n = prices.length;
-        dp[n][0] = 0;
-        dp[n][1] = 0;
-
-        for(int ind=n-1;ind>=0;ind--){
-            for(int buy=0;buy<=1;buy++){
-                int profit = 0;
-                if(buy == 1){
-                    profit = Math.max((-prices[ind] + dp[ind+1][0])
-                                                ,(0 + dp[ind+1][1]));
-                }
-                else{
-                    profit = Math.max((prices[ind] + dp[ind+1][1]), 
-                                    (0 + dp[ind+1][0]));
-                }
-                dp[ind][buy] = profit;
-            }
-        }
-
-        return dp[0][1];
-    }
-    */
-
-
-
-
-    /*
-    Memoization Code
-    public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int dp[][] = new int[n][2];
-        for(int row[]: dp){
-            Arrays.fill(row, -1);
-        }
-        return f(0, 1, prices, dp);
-    }
-    public int f(int ind, int buy, int prices[], int dp[][]){
-        if(ind == prices.length){
-            return 0;
-        }
-        if(dp[ind][buy] != -1){
-            return dp[ind][buy];
-        }
-        int profit = 0;
-        if(buy == 1){
-            profit = Math.max((-prices[ind] + f(ind+1, 0, prices, dp))
-                                                ,(0 + f(ind+1, 1, prices, dp)));
-        }
-        else{
-            profit = Math.max((prices[ind] + f(ind+1, 1, prices, dp)), 
-                                    (0 + f(ind+1, 0, prices, dp)));
-        }
-        return dp[ind][buy] = profit;
-    }
-    */
 }
