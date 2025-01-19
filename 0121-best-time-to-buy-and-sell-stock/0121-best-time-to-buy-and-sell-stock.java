54c89113e984cdb4d3467113951ed8a74880980c
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-01-19 08:57:54 +0530

    Time: 2 ms (76.21%), Space: 61.3 MB (86.9%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,11 +1,17 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        //initially i will buy on first day 
+        // i will keep my buy as low as possible
         int minBuy = prices[0];
+        //i will keep my profit as high as possible
         int maxProfit = 0;
         for(int price: prices){
-            int profit = price - minBuy; // 0 
-            maxProfit = Math.max(maxProfit, profit); // 0 
-            minBuy = Math.min(minBuy, price); // 7 
+            //i will check if i get maxprofit everyday 
+            int profit = price - minBuy; 
+            //i will store the maxprofit as high as possible
+            maxProfit = profit>maxProfit? profit: maxProfit;
+            // i will update min buy everyday
+            minBuy = minBuy>price? price: minBuy;
         }
         return maxProfit;
     }
