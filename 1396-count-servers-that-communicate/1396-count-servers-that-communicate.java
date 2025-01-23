d342f7433da8003de3bb68ad01661687ff475f23
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-01-23 07:26:59 +0530

    Time: 3 ms (47.43%), Space: 53.1 MB (5.14%) - LeetHub

--- 1396-count-servers-that-communicate/1396-count-servers-that-communicate.java
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.java
@@ -2,13 +2,7 @@ class Solution {
     public int countServers(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        int pre[] = new int[n];
-        //row check 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < grid[i].length; j++){
-                pre[i] += grid[i][j];
-            }
-        }
+        
         //col check
         int preCol[] = new int[m];
         for(int j = 0; j < m; j++){
@@ -18,14 +12,23 @@ class Solution {
         }
 
         int count = 0;
-        //check can communicate for each server
+
+        int pre[] = new int[n];
+        //row check 
         for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                pre[i] += grid[i][j];
+            }
             for(int j = 0; j < m ; j++){
                 if(grid[i][j] == 1 && Math.max(pre[i], preCol[j]) > 1){
                     count++;
                 }
             }
         }
+
+
         return count;
+       
+        //TC:- O(n*m) + O(n*m) + O(n*m)
     }
 }
