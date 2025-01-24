3a6c50ba3dda9bf0e80e40b30d921e1bb0730335
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-01-24 19:09:53 +0530

    Time: 8 ms (24.26%), Space: 42.6 MB (21.82%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.java
@@ -1,5 +1,25 @@
 class Solution {
-    //Memoization Solution
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        int n = arr.length;
+        int dp[] = new int[n+1];
+        for(int ind = n-1; ind >=0; ind--){
+            int len = 0; 
+            int maxi = 0;
+            int maxAns = 0;
+            for(int i = ind; i < Math.min(ind + k, n); i++){
+                len++;
+                maxi = Math.max(maxi, arr[i]);
+                int sum = (len * maxi) + dp[i+1];
+                maxAns = Math.max(maxAns, sum);
+            }
+            dp[ind] = maxAns;
+        }
+        return dp[0];
+    }
+
+
+    /*
+    //Memoization Solution Beats 95% Let's Make it Still Better
     public int maxSumAfterPartitioning(int[] arr, int k) {
         int n = arr.length;
         int dp[] = new int[n+1];
@@ -22,4 +42,27 @@ class Solution {
         }
         return dp[ind] = maxAns;
     }
+
+    */
+    /*
+    //Recursive Solution Got TLE at 14/52 Let's Memoize it.
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        return f(0, arr, k);
+    }
+
+    public int f(int ind, int arr[], int k){
+        int n = arr.length;
+        if(ind == n) return 0;
+        int len = 0; 
+        int maxi = 0;
+        int maxAns = 0;
+        for(int i = ind; i < Math.min(ind + k, n); i++){
+            len++;
+            maxi = Math.max(maxi, arr[i]);
+            int sum = (len * maxi) + f(i+1, arr, k);
+            maxAns = Math.max(maxAns, sum);
+        }
+        return maxAns;
+    }
+    */
 }
