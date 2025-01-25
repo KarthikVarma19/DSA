cd7a375f2b834f0868a911324e402e2d8dcd682f
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-01-25 09:27:32 +0530

    Time: 768 ms (48%), Space: 41.5 MB (83.35%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,29 +1,58 @@
 class Solution {
-    public boolean isPalindrome(String s){
-        int i = 0, j = s.length() - 1;
-        while(i < j){
-            if(s.charAt(i) != s.charAt(j)){
-                return false;
+    //In, Recursion Solution I Got 24/37 testcases passed. Let's Memoize it 
+    public int minCut(String s) {
+        int n = s.length();
+        int dp[] = new int[n+1];
+        Arrays.fill(dp, -1);
+        return f(0, s.toCharArray(), dp) - 1;
+    }
+    //Helper Function Tells the minimum cuts needed to make palindrome partition
+    public int f(int i, char s[], int dp[]){
+        if(i == s.length) return 0;
+        if(dp[i] != -1) return dp[i];
+        int mini = Integer.MAX_VALUE;
+        for(int ind = i; ind < s.length; ind++){
+            if(isPalindrome(i, ind, s)){
+                int cuts = 1 + f(ind+1, s, dp);
+                mini = Math.min(mini, cuts);
             }
+        }
+        return dp[i] = mini;
+    }
+    //Function Tells if a String is palindrome or not in range(i, j)
+    public boolean isPalindrome(int i, int j, char s[]){
+        while(i<j){
+            if(s[i] != s[j]) return false;
             i++;
             j--;
         }
         return true;
     }
+    /*
+    //In, Recursion Solution I Got 24/37 testcases passed. Let's Memoize it 
     public int minCut(String s) {
-        int dp[] = new int[s.length()+1];
-        for(int ind = s.length()-1; ind >= 0; ind--){
-            StringBuilder sb = new StringBuilder("");
-            int mini = Integer.MAX_VALUE;
-            for(int j = ind; j < s.length(); j++){
-                sb.append(s.charAt(j));
-                if(isPalindrome(sb.toString())){
-                    int cost = 1 + dp[j+1];
-                    mini = Math.min(cost, mini);
-                }
-            }
-            dp[ind] = mini;
-        }
-        return dp[0]-1;
+        return f(0, s.toCharArray()) - 1;
     }
+    //Helper Function Tells the minimum cuts needed to make palindrome partition
+    public int f(int i, char s[]){
+        if(i == s.length) return 0;
+        int mini = Integer.MAX_VALUE;
+        for(int ind = i; ind < s.length; ind++){
+            if(isPalindrome(i, ind, s)){
+                int cuts = 1 + f(ind+1, s);
+                mini = Math.min(mini, cuts);
+            }
+        }
+        return mini;
+    }
+    //Function Tells if a String is palindrome or not in range(i, j)
+    public boolean isPalindrome(int i, int j, char s[]){
+        while(i<j){
+            if(s[i] != s[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    */
 }
