7832b6aa0cae53c1e95ab651d60a93cd383564f0
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-01-25 16:02:49 +0530

    Time: 0 ms (100%), Space: 41.6 MB (63.88%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int open = 0;
+        int adds = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '(') open++;
+            else {
+                if(open > 0) open--;
+                else adds++;
+            }
+        }
+        
+        return open + adds;
+    }
+}
