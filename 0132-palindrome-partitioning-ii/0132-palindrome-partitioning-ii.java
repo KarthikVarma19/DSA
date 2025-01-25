1568772fda157ca3fbdc1eb084cfe6c39202dbed
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-01-25 09:37:37 +0530

    Time: 758 ms (48.15%), Space: 42.1 MB (64.56%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,24 +1,23 @@
 class Solution {
-    //In, Memoization Solution It Beats 48.00% Let's Tabulize it 
+    //In, Tabulation Solution It Beats
+    //It is a Bottom Up Approach 
+    //Since, in top down approach f(i) goes from 0 to n
+    //So, Bottom Up Approach f(i) goes from n to 0;
     public int minCut(String s) {
         int n = s.length();
         int dp[] = new int[n+1];
-        Arrays.fill(dp, -1);
-        //-1 Because It Cut's at last index as of our solutin does
-        return f(0, s.toCharArray(), dp) - 1;
-    }
-    //Helper Function Tells the minimum cuts needed to make palindrome partition
-    public int f(int i, char s[], int dp[]){
-        if(i == s.length) return 0;
-        if(dp[i] != -1) return dp[i];
-        int mini = Integer.MAX_VALUE;
-        for(int ind = i; ind < s.length; ind++){
-            if(isPalindrome(i, ind, s)){
-                int cuts = 1 + f(ind+1, s, dp);
-                mini = Math.min(mini, cuts);
+        char str[] = s.toCharArray();
+        for(int i = n-1; i >= 0; i--){
+            int mini = Integer.MAX_VALUE;
+            for(int ind = i; ind < str.length; ind++){
+                if(isPalindrome(i, ind, str)){
+                    int cuts = 1 + dp[ind+1];
+                    mini = Math.min(mini, cuts);
+                }
             }
+            dp[i] = mini;
         }
-        return dp[i] = mini;
+        return dp[0] - 1;
     }
     //Function Tells if a String is palindrome or not in range(i, j)
     public boolean isPalindrome(int i, int j, char s[]){
