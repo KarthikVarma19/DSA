2aba57e9f7ed11e04a4a0c9fac6a2fa354cea7ba
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-01-25 09:28:46 +0530

    Time: 772 ms (47.85%), Space: 41.3 MB (94.42%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -1,9 +1,10 @@
 class Solution {
-    //In, Recursion Solution I Got 24/37 testcases passed. Let's Memoize it 
+    //In, Memoization Solution It Beats 48.00% Let's Tabulize it 
     public int minCut(String s) {
         int n = s.length();
         int dp[] = new int[n+1];
         Arrays.fill(dp, -1);
+        //-1 Because It Cut's at last index as of our solutin does
         return f(0, s.toCharArray(), dp) - 1;
     }
     //Helper Function Tells the minimum cuts needed to make palindrome partition
@@ -28,31 +29,4 @@ class Solution {
         }
         return true;
     }
-    /*
-    //In, Recursion Solution I Got 24/37 testcases passed. Let's Memoize it 
-    public int minCut(String s) {
-        return f(0, s.toCharArray()) - 1;
-    }
-    //Helper Function Tells the minimum cuts needed to make palindrome partition
-    public int f(int i, char s[]){
-        if(i == s.length) return 0;
-        int mini = Integer.MAX_VALUE;
-        for(int ind = i; ind < s.length; ind++){
-            if(isPalindrome(i, ind, s)){
-                int cuts = 1 + f(ind+1, s);
-                mini = Math.min(mini, cuts);
-            }
-        }
-        return mini;
-    }
-    //Function Tells if a String is palindrome or not in range(i, j)
-    public boolean isPalindrome(int i, int j, char s[]){
-        while(i<j){
-            if(s[i] != s[j]) return false;
-            i++;
-            j--;
-        }
-        return true;
-    }
-    */
 }
