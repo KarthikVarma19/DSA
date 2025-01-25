fbb7a26cf51cb89b19baeaf256e2a9cc54e2c564
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-01-25 15:13:27 +0530

    Time: 0 ms (100%), Space: 41.6 MB (85.38%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,37 +1,20 @@
 class Solution {
     public int numDecodings(String s) {
         int n = s.length();
-        int dp[] = new int[n];
-        Arrays.fill(dp, -1);
-        return f(0, s, dp);
-    }
-    public int f(int ind, String s, int dp[]){
-        int n = s.length();
-        if(ind == n) return 1;
-        if(s.charAt(ind) == '0') return 0;
-        if(dp[ind] != -1) return dp[ind];
-        int ways = 0;
-        ways += f(ind+1, s, dp);
-        if(ind < n-1 && ((s.charAt(ind)-'0') * 10 + (s.charAt(ind+1) - '0'))<27){
-            ways += f(ind+2, s, dp);
+        int dp[] = new int[n+1];
+        dp[n] = 1;
+        for(int ind = n-1; ind >= 0; ind--){
+            if(s.charAt(ind) == '0'){
+                dp[ind] = 0;
+                continue;
+            }
+            int ways = 0;
+            ways += dp[ind+1];
+            if(ind < n-1 && ((s.charAt(ind)-'0') * 10 + (s.charAt(ind+1) - '0'))<27){
+                ways += dp[ind+2];
+            }
+            dp[ind] = ways;
         }
-        return dp[ind] = ways;
+        return dp[0];
     }
-    /*
-    //Got TLE at 23/269
-    public int numDecodings(String s) {
-        return f(0, s);
-    }
-    public int f(int ind, String s){
-        int n = s.length();
-        if(ind == n) return 1;
-        if(s.charAt(ind) == '0') return 0;
-        int ways = 0;
-        ways += f(ind+1, s);
-        if(ind < n-1 && ((s.charAt(ind)-'0') * 10 + (s.charAt(ind+1) - '0'))<27){
-            ways += f(ind+2, s);
-        }
-        return ways;
-    }
-    */
 }
