ed2e9b413c813938fbc99d6e907c886be4e6b9a3
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-01-26 17:45:05 +0530

    Time: 0 ms (100%), Space: 41.7 MB (64.33%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,27 +1,31 @@
 class Solution {
-    //In, Memoization Solution. I Got TLE at 31/42 testcases passed 
+    //In, Tabulation Solution. It Beats 100%
     //Time Complexity:- O(m*n)
-    //Spac Complexity:- O(m+n) + O(m*n)
+    //Spac Complexity:- O(m*n)
     public int uniquePathsWithObstacles(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         if(grid[0][0] == 1) return 0;
         int dp[][] = new int[m][n];
-        for(int row[]: dp){
-            Arrays.fill(row, -1);
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 1){
+                    continue;
+                }
+                else if(i == 0 && j == 0){
+                    dp[i][j] = 1;
+                    continue;
+                }
+                int up = 0, left = 0;
+                if(i-1 >= 0){
+                    up = dp[i-1][j];
+                }
+                if(j-1 >= 0){
+                    left = dp[i][j-1];
+                }
+                dp[i][j] = (up+left);
+            }
         }
-        return f(m-1, n-1, grid, dp);
-    }
-    public int f(int i, int j, int grid[][], int dp[][]){
-        if(i == 0 && j == 0){
-            return 1;
-        }
-        if(i < 0 || j < 0 || grid[i][j] == 1){
-            return 0;
-        }
-        if(dp[i][j] != -1) return dp[i][j];
-        int up = f(i-1, j, grid, dp);
-        int left = f(i, j-1, grid, dp);
-        return dp[i][j] = (up+left);
+        return dp[m-1][n-1];
     }
 }
