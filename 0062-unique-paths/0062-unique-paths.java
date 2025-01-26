13da5a7ae8029169cad536809e33f1138ed218ae
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-01-26 17:05:42 +0530

    Time: 0 ms (100%), Space: 40.2 MB (87.1%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,42 +1,20 @@
 class Solution {
+    //In Tabulation Code, Bottom-Up Approach 
     public int uniquePaths(int m, int n) {
         int dp[][] = new int[m][n];
-        for(int each[]: dp){
-            Arrays.fill(each, -1);
+        for(int i = 0; i < m; i++){
+            dp[i][0] = 1;
         }
-        return f(m-1, n-1, dp);
+        for(int j = 0; j < n; j++){
+            dp[0][j] = 1;
+        }
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                int up = dp[i-1][j];
+                int left = dp[i][j-1];
+                dp[i][j] = up + left;
+            }
+        }
+        return dp[m-1][n-1];
     }
-
-    public int f(int i, int j, int dp[][]){
-        if(i == 0 && j == 0){
-            return 1;
-        }
-        if(i<0 || j<0){
-            return 0;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        int up = f(i-1, j, dp);
-        int left = f(i, j-1, dp);
-        return dp[i][j] = up + left;
-    }
-    /*
-    public int uniquePaths(int m, int n) {
-        return f(m-1, n-1);
-    }
-
-    public int f(int i, int j){
-        if(i == 0 && j == 0){
-            return 1;
-        }
-        if(i<0 || j<0){
-            return 0;
-        }
-
-        int up = f(i-1, j);
-        int left = f(i, j-1);
-        return up + left;
-    }
-    */
 }
