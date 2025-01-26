e7b34f81a884f4a01762a88f42b55e85b670856c
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-01-26 17:09:39 +0530

    Time: 0 ms (100%), Space: 40.2 MB (94.42%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,20 +1,19 @@
 class Solution {
-    //In Tabulation Code, Bottom-Up Approach 
+    //In Space Optmization Code, Bottom-Up Approach Beats 100%
+    //Time Complexity:- O(m + n) + O(m*n)
+    //Space Complexity:- O(m * n)
     public int uniquePaths(int m, int n) {
-        int dp[][] = new int[m][n];
-        for(int i = 0; i < m; i++){
-            dp[i][0] = 1;
-        }
+        int dp[] = new int[n];
         for(int j = 0; j < n; j++){
-            dp[0][j] = 1;
+            dp[j] = 1;
         }
         for(int i = 1; i < m; i++){
             for(int j = 1; j < n; j++){
-                int up = dp[i-1][j];
-                int left = dp[i][j-1];
-                dp[i][j] = up + left;
+                int up = dp[j];
+                int left = dp[j-1];
+                dp[j] = up + left;
             }
         }
-        return dp[m-1][n-1];
+        return dp[n-1];
     }
 }
