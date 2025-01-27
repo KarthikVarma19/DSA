ebd998453bf8ac5400a62f7656f3991c1cc4636d
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-01-27 07:17:28 +0530

    Time: 39 ms (14.46%), Space: 45.7 MB (11.37%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        long dp[][] = new long[n][10000];
+        for(long row[]: dp){
+            Arrays.fill(row, -1);
+        }
+        long noOfCoins = f(n-1, coins, amount, dp);
+        if(noOfCoins == Integer.MAX_VALUE) return -1;
+        return (int)noOfCoins;
+    }
+    public long f(int ind, int coins[], int amount, long dp[][]){
+        if(ind == 0){
+            if(amount%coins[0] == 0){
+                return (amount / coins[0]);
+            }
+            return Integer.MAX_VALUE;
+        }
+        if(dp[ind][amount] != -1) return dp[ind][amount];
+        long notTake = f(ind-1, coins, amount, dp);
+        long take = Integer.MAX_VALUE;
+        if(amount >= coins[ind]){
+            take = 1 + f(ind, coins, amount-coins[ind], dp);
+        }
+        return dp[ind][amount] = Math.min(take, notTake);
+    }
+}
