095cc391bb92130d4ccc4c874c6d5ea7889a21ee
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-01-27 07:26:15 +0530

    Time: 19 ms (42.03%), Space: 45.5 MB (15.3%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,26 +2,26 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
         long dp[][] = new long[n][amount+1];
-        for(long row[]: dp){
-            Arrays.fill(row, -1);
+        for(int am = 0; am <= amount; am++){
+            if(am%coins[0] == 0){
+                dp[0][am] = (am / coins[0]);
+            }
+            else{
+                dp[0][am] = Integer.MAX_VALUE;
+            }  
         }
-        long noOfCoins = f(n-1, coins, amount, dp);
+        for(int ind = 1; ind < n; ind++){
+            for(int am = 0; am <= amount; am++){
+                long notTake = dp[ind-1][am];
+                long take = Integer.MAX_VALUE;
+                if(am >= coins[ind]){
+                    take = 1 + dp[ind][am-coins[ind]];
+                }
+                dp[ind][am] = Math.min(take, notTake);
+            }
+        }
+        long noOfCoins = dp[n-1][amount];
         if(noOfCoins == Integer.MAX_VALUE) return -1;
         return (int)noOfCoins;
-    }
-    public long f(int ind, int coins[], int amount, long dp[][]){
-        if(ind == 0){
-            if(amount%coins[0] == 0){
-                return (amount / coins[0]);
-            }
-            return Integer.MAX_VALUE;
-        }
-        if(dp[ind][amount] != -1) return dp[ind][amount];
-        long notTake = f(ind-1, coins, amount, dp);
-        long take = Integer.MAX_VALUE;
-        if(amount >= coins[ind]){
-            take = 1 + f(ind, coins, amount-coins[ind], dp);
-        }
-        return dp[ind][amount] = Math.min(take, notTake);
     }
 }
