02d74df84027b327344fecb4a16ffdb6eba931ea
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-01-31 07:55:39 +0530

    Time: 148 ms (8.76%), Space: 88.4 MB (5.06%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.java
+++ 0403-frog-jump/0403-frog-jump.java
@@ -1,38 +1,30 @@
 class Solution {
-    int n;
     public boolean canCross(int[] stones) {
-        n = stones.length;
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        for(int i = 0; i < stones.length; i++){
-            hm.put(stones[i], i);
-        }
+        if(stones[1] != 1) return false;
         int dp[][] = new int[2001][2001];
         for(int row[]: dp){
             Arrays.fill(row, -1);
         }
-        return f(0, 0, hm, stones, dp); 
-
+        return f(1, 1, stones, dp); 
     }
-    public boolean f(int curStoneIndex, int prevJump, HashMap<Integer,Integer> hm , int stones[], int dp[][]){
-        if(curStoneIndex == n-1) return true;
-        if(dp[curStoneIndex][prevJump] != -1){
-            return dp[curStoneIndex][prevJump] == 1;
+    public boolean f(int i, int k, int stones[], int dp[][]){
+        if(i == stones.length-1) return true;
+        if(dp[i][k] != -1){
+            return dp[i][k] == 1;
         }
-        boolean result = false;
-        for(int nextJump = prevJump-1; nextJump <= prevJump+1; nextJump++){
-            if(nextJump > 0){
-                int nextStone = stones[curStoneIndex] + nextJump;
-                if(hm.containsKey(nextStone) == true){
-                    int nextStoneIndex = hm.get(nextStone);
-                    result = result | f(nextStoneIndex, nextJump, hm, stones, dp);
+        for(int j = i + 1; j < stones.length; j++){
+            int gap = stones[j] - stones[i];
+            if(gap <= k + 1 && gap >= k - 1){
+                if(f(j, gap, stones, dp)){
+                    dp[i][k] = 1;
+                    return true;
                 }
             }
+            else if(gap > k + 1){
+                break;
+            }
         }
-        if(result == true){
-            dp[curStoneIndex][prevJump] =  1;
-            return true;
-        }
-        dp[curStoneIndex][prevJump] = 0;
+        dp[i][k] = 0;
         return false;
     }
 }
