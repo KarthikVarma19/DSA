9663461e17782f8af0ad51fb4ff7fefa1ec042b0
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-02-02 18:26:53 +0530

    Time: 1 ms (86.86%), Space: 44.6 MB (33.14%) - LeetHub

--- 0517-super-washing-machines/0517-super-washing-machines.java
+++ 0517-super-washing-machines/0517-super-washing-machines.java
@@ -10,22 +10,30 @@ class Solution {
         if(target < 1 || tot % n != 0) return -1;
         int move[] = new int[n];
         int result = 0;
+        int prev = 0; 
+        int next = 0;
         for(int i = 0; i < n - 1; i++){
             //grabbing from next machine it's his responsibility to donate
             if(machines[i] < target){
                 machines[i+1] -= target - machines[i];
-                move[i+1] = target - machines[i];
+                next = target - machines[i];
                 machines[i] = target;
-                result = Math.max(result, move[i+1]);
+                result = Math.max(result, next);
+                prev = next;
+                next = 0;
             }
             //donating to the next machine it's my responsibility to donate
             else{
-                move[i] += machines[i] - target;
+                prev += machines[i] - target;
                 machines[i+1] += machines[i] - target;
                 machines[i] = target;
-                result = Math.max(result, move[i]);
+                result = Math.max(result, prev);
+                prev = 0;
+                next = 0;
             }
+
         }
         return result;
+
     }
 }
