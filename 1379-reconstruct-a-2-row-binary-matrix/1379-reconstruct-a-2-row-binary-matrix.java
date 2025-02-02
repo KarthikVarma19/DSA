aa5a86bfb4aef38f1ba715383352ccfbae0b3703
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-02-02 19:31:03 +0530

    Time: 5 ms (100%), Space: 64.7 MB (5.66%) - LeetHub

--- 1379-reconstruct-a-2-row-binary-matrix/1379-reconstruct-a-2-row-binary-matrix.java
+++ 1379-reconstruct-a-2-row-binary-matrix/1379-reconstruct-a-2-row-binary-matrix.java
@@ -1,47 +1,31 @@
 class Solution {
     public List<List<Integer>> reconstructMatrix(int upper, int lower, int[] colsum) {
-        
-        List<List<Integer>> ans = new ArrayList<>();
-        ans.add(new ArrayList<Integer>());
-        ans.add(new ArrayList<Integer>());
-        int setBits = 0;
-        //First Giving Priority to 2colsum 
-        for(int i = 0; i < colsum.length; i++)
+        int n = colsum.length;
+        int mat[][] = new int[2][n];
+        for(int i = 0; i < n; i++)
         {
-            ans.get(0).add(0);
-            ans.get(1).add(0);
-            if(colsum[i] == 2){
-                colsum[i] = 0;
+            if(colsum[i] == 0)
+                continue;
+            else if(colsum[i] == 2)
+            {
                 lower -= 1;
                 upper -= 1;
-                ans.get(0).set(i, 1);
-                ans.get(1).set(i, 1);
+                mat[0][i] = 1;
+                mat[1][i] = 1;
             }
-            setBits += colsum[i];
-            if((upper < 0) || (lower < 0)){
-                List<List<Integer>> empty = new ArrayList<>();
-                return empty;
-            }
-        }
-        //Invalid Case
-        if((upper + lower) != setBits)
-        {
-            List<List<Integer>> empty = new ArrayList<>();
-            return empty;
-        }
-        //Traversing and filling Upper Row and then Lower Row
-        for(int col = 0; col < colsum.length; col++){
-            if(upper > 0 && colsum[col] > 0){
-                ans.get(0).set(col, 1);
-                colsum[col] -= 1;
+            else if(upper > lower)
+            {
+                mat[0][i] = 1;
                 upper -= 1;
             }
-            if(lower > 0 && colsum[col] > 0){
-                ans.get(1).set(col, 1);
-                colsum[col] -= 1;
+            else{
+                mat[1][i] = 1;
                 lower -= 1;
             }
         }
-        return ans;
+        if(upper != 0 || lower != 0){
+            return new ArrayList<>();
+        }
+        return new ArrayList(Arrays.asList(mat[0], mat[1]));
     }
 }
