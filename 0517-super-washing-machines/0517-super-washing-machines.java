01c57ad2254a1bd229470e332fb367afa108f32b
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-02-02 18:20:57 +0530

    Time: 1 ms (86.86%), Space: 44.8 MB (5.14%) - LeetHub

---
+++ 0517-super-washing-machines/0517-super-washing-machines.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int findMinMoves(int[] machines) {
+        int n = machines.length;
+        int tot = 0;
+        for(int d: machines){
+            tot += d;
+        }
+        if(tot == 0 || n == 1) return 0;
+        int target = tot/n;
+        if(target < 1 || tot % n != 0) return -1;
+        int move[] = new int[n];
+        int result = 0;
+        for(int i = 0; i < n - 1; i++){
+            //grabbing from next machine it's his responsibility to donate
+            if(machines[i] < target){
+                machines[i+1] -= target - machines[i];
+                move[i+1] = target - machines[i];
+                machines[i] = target;
+                result = Math.max(result, move[i+1]);
+            }
+            //donating to the next machine it's my responsibility to donate
+            else{
+                move[i] += machines[i] - target;
+                machines[i+1] += machines[i] - target;
+                machines[i] = target;
+                result = Math.max(result, move[i]);
+            }
+        }
+        return result;
+    }
+}
