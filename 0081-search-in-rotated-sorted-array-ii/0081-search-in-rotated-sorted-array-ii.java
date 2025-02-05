edbc25a1495fd813bc934716fe2a3cd1dd4c5eb8
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-02-05 07:46:56 +0530

    Time: 0 ms (100%), Space: 42.9 MB (96.45%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -4,28 +4,39 @@ class Solution {
         int low = 0;
         int high = n - 1;
         while(low <= high){
+            //to control overflow
             int mid = low + (high-low)/2;
+            //if array contains duplicates
             if(arr[low] == arr[mid] && arr[mid] == arr[high]){
-                if(arr[low] == target) return true;
-                low++;
-                high--;
+                //we may loose on of the candidate so checking if one of them is our target
+                if(arr[low] == target){
+                    return true;
+                }
+                low += 1;
+                high -= 1;
             }
+            //if mid is my target
             else if(arr[mid] == target){
                 return true;
             }
             //left is sorted
             else if(arr[low] <= arr[mid]){
+                //if target lies left side
                 if(arr[low] <= target && target <= arr[mid]){
                     high = mid - 1;
                 }
+                //not then target lies right side
                 else{
                     low = mid + 1;
                 }
             }
+            //right is sorted
             else{
+                //if target lies right side
                 if(arr[mid] <= target && target <= arr[high]){
                     low = mid + 1;
                 }
+                //not then target lies in left side
                 else{
                     high = mid - 1;
                 }
