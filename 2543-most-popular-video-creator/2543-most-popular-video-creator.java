8bf56e48cce7a47ac81d657852a5773abbf6356e
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-02-08 17:14:15 +0530

    Time: 56 ms (56.34%), Space: 118.7 MB (11.27%) - LeetHub

--- 2543-most-popular-video-creator/2543-most-popular-video-creator.java
+++ 2543-most-popular-video-creator/2543-most-popular-video-creator.java
@@ -1,10 +1,17 @@
 class Solution {
-    class Video{
+    class Video implements Comparable<Video> {
         String id;
         int view;
         Video(String i, int v){
             id = i;
             view = v;
+        }
+        @Override
+        public int compareTo(Video a){
+            if(a.view != this.view){
+                return a.view - this.view; //Sort it in Highest View Count First
+            }
+            return (this.id).compareTo(a.id); //if View's are equal Sort it lexicographically
         }
     }
     HashMap<String, Long> popCreators;
@@ -14,25 +21,19 @@ class Solution {
         int n = creators.length;
         popCreators = new HashMap<>();
         creatorsPopVideos = new HashMap<>();
+
         //Inserting Video's as Per Need
         for(int i = 0; i < n; i++){
             String creator = creators[i];
             String id = ids[i];
             int view = views[i];
             popCreators.put(creator, popCreators.getOrDefault(creator, (long)0) + view);
-            creatorsPopVideos.putIfAbsent(creator, new PriorityQueue<Video>(new Comparator<Video>(){
-                @Override
-                public int compare(Video a, Video b){
-                    if(a.view != b.view){
-                        return b.view - a.view; //Sort it in Highest View Count First
-                    }
-                    return (a.id).compareTo(b.id); //if View's are equal Sort it lexicographically
-                }
-            }));
+            creatorsPopVideos.putIfAbsent(creator, new PriorityQueue<>());
             Video newVideo = new Video(id, view);
             PriorityQueue<Video> pq = creatorsPopVideos.get(creator);
             pq.add(newVideo);
         }
+
         //Retreving Popular Video Creator's
         ArrayList<String> creatorsList =  new ArrayList<>();
         long mostViews = -1;
