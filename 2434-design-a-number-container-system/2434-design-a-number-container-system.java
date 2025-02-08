e6029adc8f645d2bc5b01fb84809c489eb9974d6
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-02-08 08:34:38 +0530

    Time: 54 ms (98.63%), Space: 95.7 MB (96.58%) - LeetHub

--- 2434-design-a-number-container-system/2434-design-a-number-container-system.java
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.java
@@ -1,41 +1,40 @@
 class NumberContainers {
 
-    HashMap<Integer,Integer> container;
-    HashMap<Integer,TreeSet<Integer>> minIndex;
-
+    private HashMap<Integer,Integer> indexToNumber;
+    private HashMap<Integer,PriorityQueue<Integer>> numberToMinIndex;
+    
     public NumberContainers() {
-        container = new HashMap<>();
-        minIndex = new HashMap<>();
+        indexToNumber = new HashMap<>();
+        numberToMinIndex = new HashMap<>();
     }
     
     public void change(int index, int number) {
-        //Container
-        if(container.containsKey(index)){
-            int prev = container.get(index);
-            TreeSet<Integer> pq = minIndex.get(prev);
-            //Remove the Index From PrevNumber
-            pq.remove(index);
-            //Delete the Container
-            if(pq.size() == 0){
-                container.remove(index);
-            }
+        indexToNumber.put(index, number);
+        if(numberToMinIndex.containsKey(number) == false){
+            numberToMinIndex.put(number, new PriorityQueue<>());
         }
-        container.put(index, number);
-        //MinIndex
-        if(minIndex.containsKey(number) == false){
-            minIndex.put(number, new TreeSet<>());
-        }
-        TreeSet<Integer> pq = minIndex.get(number);
-        pq.add(index);
+        numberToMinIndex.get(number).add(index);
     }
     
     public int find(int number) {
-        if(minIndex.containsKey(number) == false){
+        if(!numberToMinIndex.containsKey(number)){
             return -1;
         }
-        TreeSet<Integer> pq = minIndex.get(number);
-        if(pq.size() == 0) return -1;
-        int min = pq.first();
-        return min;
+        PriorityQueue<Integer> minIndicies = numberToMinIndex.get(number);
+        while(!minIndicies.isEmpty()){
+            int peek = minIndicies.peek();
+            if(indexToNumber.get(peek) == number){
+                return peek;
+            }
+            minIndicies.poll();
+        }
+        return -1;
     }
 }
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers obj = new NumberContainers();
+ * obj.change(index,number);
+ * int param_2 = obj.find(number);
+ */
