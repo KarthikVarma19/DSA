9a5728ee68837e1082a1d532619e2acd79da9391
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-02-09 11:55:43 +0530

    Time: 6 ms (100%), Space: 64 MB (100%) - LeetHub

--- 3760-assign-elements-to-groups-with-constraints/3760-assign-elements-to-groups-with-constraints.java
+++ 3760-assign-elements-to-groups-with-constraints/3760-assign-elements-to-groups-with-constraints.java
@@ -1,20 +1,22 @@
 class Solution {
     public int[] assignElements(int[] groups, int[] elements) {
+
         int maxi = 0;
         for(int g: groups) maxi = Math.max(maxi, g);
         int g[] = new int[maxi+1];
         Arrays.fill(g, -1);
-        int v[] = new int[maxi+1];
+        //int v[] = new int[maxi+1];
+
         for(int i = 0; i < elements.length; i++){
             int ele = elements[i];
-            if(ele > maxi || v[ele] == 1){
+            if(ele > maxi || g[ele] != -1){
                 continue;
             }
-            v[ele] = 1;
+            //v[ele] = 1;
             for(int mul=ele; mul <= maxi; mul += ele){
                 if(g[mul] == -1){
                     g[mul] = i;
-                    v[mul] = 1;
+                    //v[mul] = 1;
                 }
             }
         }
