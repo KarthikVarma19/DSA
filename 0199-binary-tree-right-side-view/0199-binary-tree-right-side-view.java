0b91ab733dd35e20ffc76b747d1844a92ae3582e
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-02-10 20:47:35 +0530

    Time: 0 ms (100%), Space: 41.8 MB (95.69%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,36 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    class Pair{
-        TreeNode node;
-        int level;
-        Pair(TreeNode node, int level){
-            this.node = node;
-            this.level = level;
-        }
-    }
+    List<Integer> ans = new ArrayList<>();
     public List<Integer> rightSideView(TreeNode root) {
+        solve(root, 0);
+        return ans;
+    }
+    public void solve(TreeNode root, int level){
         if(root == null){
-            return new ArrayList<>();
+            return;
         }
-        //Level Number -> Last Node in that Level
-        Map<Integer,Integer> map = new TreeMap<>();        
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(root, 0));
-        while(!q.isEmpty()){
-            Pair p = q.remove();
-            int curLevel = p.level;
-            TreeNode curNode = p.node;
-            map.put(curLevel, curNode.val);
-            if(curNode.left != null){
-                q.add(new Pair(curNode.left, curLevel+1));
-            }
-            if(curNode.right != null){
-                q.add(new Pair(curNode.right, curLevel+1));
-            }
+        if(ans.size() == level){
+            ans.add(root.val);
         }
-        List<Integer> rightView = new ArrayList<>();
-        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
-            rightView.add(entry.getValue());
-        }
-        return rightView;
+        solve(root.right, level+1);
+        solve(root.left, level+1);
     }
 }
