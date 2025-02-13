928b98416b076b206a90b18cd1affd105b3d9701
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-02-13 06:42:25 +0530

    Time: 151 ms (45.27%), Space: 71 MB (17.28%) - LeetHub

---
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/3332-minimum-operations-to-exceed-threshold-value-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        for(long num: nums){
+            pq.add(num);
+        }
+        int ops = 0;
+        while(pq.size() > 1 && pq.peek() < k){
+            long e1 = pq.poll(), e2 = pq.poll();
+            pq.add(Math.min(e1,e2)*2+Math.max(e1,e2));
+            ops += 1;
+        }
+        return ops;
+    }
+}
