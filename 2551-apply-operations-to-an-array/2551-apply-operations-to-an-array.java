a2de785aeb4c8340ec9254392bcb235de6749fd9
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-03-01 07:51:37 +0530

    Time: 1 ms (96.37%), Space: 44.2 MB (17.11%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -2,31 +2,17 @@ class Solution {
     public int[] applyOperations(int[] nums) {
         //Applying the Given Operation
         int n = nums.length;
-        for(int i = 0; i < n-1; i++){
-            if(nums[i] == nums[i+1]){
+        int ans[] = new int[n];
+        int ap = 0;
+        for(int i = 0; i < n; i++){
+            if(i < n-1 && nums[i] == nums[i+1]){
                 nums[i] *= 2;
                 nums[i+1] = 0;
             }
+            if(nums[i] != 0){
+                ans[ap++] = nums[i];
+            }
         }
-        //Move Zero's to the end
-        int zp = 0, np = 0;
-        while(zp < n && np < n){
-            //find the first zero 
-            while(zp < n && nums[zp] != 0){
-                zp++;
-            }
-            //find the next non - zero number
-            np = zp + 1;
-            while(np < n && nums[np] == 0){
-                np++;
-            }
-            //swap them
-            if(zp < n && np < n){
-                nums[zp] = nums[np];
-                nums[np] = 0;
-            }
-            zp++;
-        }
-        return nums;
+        return ans;
     }
 }
