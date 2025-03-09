733529b03287d55944549fa2fc52ad1a74dd8f6f
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-03-09 22:18:12 +0530

    Time: 10 ms (89.92%), Space: 45.2 MB (12.9%) - LeetHub

--- 2831-number-of-beautiful-pairs/2831-number-of-beautiful-pairs.java
+++ 2831-number-of-beautiful-pairs/2831-number-of-beautiful-pairs.java
@@ -1,19 +1,30 @@
 class Solution {
-    
+    public void preComputeCoPrimes(boolean coprimes[][]){
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                if(gcd(i, j) == 1){
+                    coprimes[i][j] = true;
+                }
+            }
+        }
+    }
     public int countBeautifulPairs(int[] nums) {
         int n = nums.length;
+        boolean coprimes[][] = new boolean[10][10];
+        preComputeCoPrimes(coprimes);
         int beautifulPairs = 0;
         for(int i = 0; i < n; i++){
             int firstDigit = getFirstDigit(nums[i]);
             for(int j = i+1; j < n; j++){
                 int lastDigit = getLastDigit(nums[j]);
-                if(gcd(firstDigit, lastDigit) == 1){
+                if(coprimes[firstDigit][lastDigit] == true){
                     beautifulPairs++;
                 }
             }
         }   
         return beautifulPairs;
     }
+
     public int getLastDigit(int num){
         int last = num % 10;
         return last;
