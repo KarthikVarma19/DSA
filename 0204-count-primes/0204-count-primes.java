0041eb33b34770740e35534d44c2f1f310fad613
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-03-09 12:32:29 +0530

    Time: 121 ms (19.47%), Space: 64.4 MB (12.91%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countPrimes(int n) {
+        if(n <= 1) return 0;
+        int seive[] = new int[n+1];
+        seive[1] = 1;
+        int count = 0;
+        for(int i = 2; i < n; i++){
+            if(seive[i] == 0){
+                count++;
+                for(int j = i + i; j < n; j = j + i){
+                    seive[j] = 1;
+                }
+            }
+        }
+        return count;
+    }
+}
