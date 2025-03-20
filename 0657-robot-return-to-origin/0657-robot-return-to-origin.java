d021ed8a9fab01ac14d164f9689d0f31a6013678
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-03-20 20:45:03 +0530

    Time: 4 ms (97%), Space: 44.7 MB (48.98%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int i = 0, j = 0;
+        for(char ch: moves.toCharArray()){
+            if(ch == 'U'){
+                i++;
+            }
+            else if(ch == 'D'){
+                i--;
+            }
+            else if(ch == 'L'){
+                j++;
+            }
+            else if(ch == 'R'){
+                j--;
+            }
+        }
+        return (i == 0 && j == 0);
+    }
+}
