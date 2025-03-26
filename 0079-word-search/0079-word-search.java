4a3c524fd78225557218724645d57b27fc2f848f
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-03-26 16:35:47 +0530

    Time: 142 ms (63.91%), Space: 42 MB (32.31%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,16 +1,15 @@
 class Solution {
-    public boolean visited[][];
     public boolean exist(char[][] board, String word) {
         int m = board.length, n = board[0].length;
-        visited = new boolean[m][n];
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(board[i][j] == word.charAt(0)){
-                    visited[i][j] = true;
+                    char ch = word.charAt(0);
+                    board[i][j] = '.';
                     if(solve(1, board, word, i, j) == true){
                         return true;
                     }
-                    visited[i][j] = false;
+                    board[i][j] = ch;
                 }
             }
         }
@@ -25,10 +24,10 @@ class Solution {
         for(int i = 0; i < 4; i++){
             int newRow = row + dir[i][0], newCol = col + dir[i][1];
             if(newRow >= 0 && newRow < board.length && newCol >= 0 && newCol < board[0].length){
-                if(board[newRow][newCol] == ch && visited[newRow][newCol] == false){
-                    visited[newRow][newCol] = true;
+                if(board[newRow][newCol] == ch && board[newRow][newCol] != '.'){
+                    board[newRow][newCol] = '.';
                     if(solve(index+1, board, word, newRow, newCol) == true) return true;
-                    visited[newRow][newCol] = false;
+                    board[newRow][newCol] = ch;
                 }
             }
         }
