a52828a748a6bfc348795aeab7863ffadcbc715a
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-04-09 17:31:22 +0530

    Time: 8 ms (60.95%), Space: 42.6 MB (51.27%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.java
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -10,21 +10,21 @@ class Solution {
     
     private String helper(int num){
 
-        if(num < 20){
+        if(num < 20){ // Between 1 to 19
             return LESS_THAN_20[num];
         }
-        if(num < 100){ // 76
+        if(num < 100){ // Between twenty to 99
             return TENS[num/10] + (((num%10)==0)? "": " " + helper(num%10));
         }
-        if(num < 1000){ // 899
+        if(num < 1_000){ //Between 100 to 999
             return helper(num/100) + " Hundred" +  (((num%100)==0)? "": " " + helper(num%100));
         }
-        if(num < 1000000){ // 99,999
-            return helper(num/1000) + " Thousand" +  (((num%1000)==0)? "": " " + helper(num%1000));
+        if(num < 1_000_000){ // Between 1000 to 9,99,999
+            return helper(num/1_000) + " Thousand" +  (((num%1000)==0)? "": " " + helper(num%1000));
         }
-        if(num < 1000000000){
-            return helper(num/1000000) + " Million" + (((num%1000000)==0)? "": " " + helper(num%1000000));
+        if(num < 1_000_000_000){ // Between 1_000_000 to 99,999,999
+            return helper(num/1_000_000) + " Million" + (((num%1000000)==0)? "": " " + helper(num%1000000));
         }
-        return helper(num/1000000000) + " Billion" + (((num%1000000000)==0)? "": " " + helper(num%1000000000));
+        return helper(num/1_000_000_000) + " Billion" + (((num%1000000000)==0)? "": " " + helper(num%1000000000));
     }
 }
