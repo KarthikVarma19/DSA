a7d4e5ff70df0e5c1bd2d48646938ec032edcfe3
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-04-16 07:39:20 +0530

    Time: 18 ms (60.03%), Space: 44.1 MB (49.76%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,25 +1,36 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        Set<List<Integer>> ans = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums); // since we are using two pointers approach
         int n = nums.length;
         for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
             for(int j = i+1; j < n; j++){
-                Set<Long> hs = new HashSet<>();
-                for(int k = j+1; k < n; k++){
-                    long sum = nums[i]+nums[j];
+                if(j > i + 1 && nums[j] == nums[j-1]) continue;
+                int k = j + 1;
+                int l = n - 1;
+                while(k < l){
+                    long sum = nums[i];
+                    sum += nums[j];
                     sum += nums[k];
-                    long rem = target - sum;
-                    if(hs.contains(rem)){
-                        List<Integer> ls = Arrays.asList(nums[i], nums[j], nums[k], (int)rem);
-                        Collections.sort(ls);
-                        // ls.sort(Integer::compareTo);
+                    sum += nums[l];
+                    if(sum == target){
+                        List<Integer> ls = Arrays.asList(nums[i], nums[j], nums[k], nums[l]);
                         ans.add(ls);
+                        k++;
+                        l--;
+                        // skip the duplicates
+                        while(k < l && nums[k] == nums[k-1]) k++;
+                        while(k < l && nums[l] == nums[l+1]) l--;
+                    }else if(sum > target){
+                        l--;
+                    }else{
+                        k++;
                     }
-                    hs.add((long)nums[k]);
                 }
+                
             }
         }
-        List<List<Integer>> finalans = new ArrayList<>(ans);
-        return finalans;
+        return ans;
     }
 }
