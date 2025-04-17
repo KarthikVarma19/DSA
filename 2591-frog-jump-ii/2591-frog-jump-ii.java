d96e3cdde27281f87e2ff7cb0b78ab4bdbec8837
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-04-17 18:33:13 +0530

    Time: 4 ms (26.51%), Space: 60.9 MB (10.23%) - LeetHub

--- 2591-frog-jump-ii/2591-frog-jump-ii.java
+++ 2591-frog-jump-ii/2591-frog-jump-ii.java
@@ -2,11 +2,9 @@ class Solution {
     public int maxJump(int[] stones) {
         int mini = Math.abs(stones[0]-stones[1]);
         int n = stones.length;
-        for(int i = 2; i < n; i+=2){
-            mini = Math.max(mini, Math.abs(stones[i]-stones[i-2]));
-        }
-        for(int i = 3; i < n; i+=2){
-            mini = Math.max(mini, Math.abs(stones[i]-stones[i-2]));
+        for(int i = 0; i < n; i++){
+            if(i-2 >=0)
+                mini = Math.max(mini, Math.abs(stones[i]-stones[i-2]));
         }
         return mini;
     }
