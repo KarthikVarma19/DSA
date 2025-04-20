41bd7f71d2da5df68363436b271f1f58363669e3
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-04-20 20:09:45 +0530

    Time: 1 ms (99.8%), Space: 44.1 MB (67.28%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,26 +1,23 @@
 class Solution {
-    //Memoization
-    public int minimumTotal(List<List<Integer>> t) {
-        int n = t.size();
-        int m = t.get(n-1).size();
-        int dp[][] = new int[m][n];
-        for(int row[]: dp){
-            Arrays.fill(row, -1);
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int m = triangle.get(n-1).size();
+        int dp[][] = new int[n][m];
+        for(int i = 0; i < n; i++){
+            Arrays.fill(dp[i], -1);
         }
-        return f(0, 0, t, dp);
+        return solve(0, 0, triangle, dp);
     }
-    public int f(int ind, int row, List<List<Integer>> t, int dp[][]){
-        if(row == t.size()-1){
-            return t.get(row).get(ind);
+    public int solve(int ind, int row, List<List<Integer>> triangle, int dp[][]){
+        if(row == triangle.size()-1){
+            return dp[ind][row] = triangle.get(row).get(ind);
         }
-
-        if(dp[ind][row] != -1) return dp[ind][row];
-
-        int choice2 = f(ind+1, row+1, t, dp);
-        int choice1 = f(ind, row+1, t, dp);
-        
-        int val = t.get(row).get(ind);
-
-        return dp[ind][row] = Math.min(choice1 , choice2) + val;
+        if(dp[ind][row] != -1){
+            return dp[ind][row];
+        }
+        int curValue = triangle.get(row).get(ind);
+        int choice1 = solve(ind, row+1, triangle, dp);
+        int choice2 = solve(ind+1, row+1, triangle, dp);
+        return dp[ind][row] =  (curValue + Math.min(choice1, choice2));
     }
 }
