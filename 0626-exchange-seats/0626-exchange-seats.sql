ae6acd34db9b0638a76b742ef080da4fe6d12398
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-04-28 11:33:49 +0530

    Time: 343 ms (83.13%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,19 +1,9 @@
-# increment the id of odd by +1 even by -1 if the id is odd and count(*) tables is odd then keep id same
-WITH table_count (total_students) AS 
-    (SELECT count(*) from Seat),
-
-    seat_swap_table (id, new_id) AS
-    (SELECT s.id,
-        CASE
-            WHEN s.id % 2 = 1 AND s.id = tc.total_students THEN s.id
-            WHEN s.id % 2 = 1 THEN s.id + 1
-            ELSE s.id - 1
-        END AS new_id
-    FROM 
-    Seat s, table_count tc)
-
-
-SELECT sst.id, st.student
-FROM seat_swap_table sst
-LEFT JOIN Seat st
-ON sst.new_id = st.id;
+SELECT 
+    CASE 
+        WHEN id % 2 = 1 AND id + 1 IN (SELECT id FROM Seat) THEN id + 1
+        WHEN id % 2 = 0 THEN id - 1
+        ELSE id
+    END AS id,
+    student
+FROM Seat
+ORDER BY id;
