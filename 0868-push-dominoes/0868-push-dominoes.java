20eca62c99bf83ea228a9d47581653593f3426dd
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-05-03 00:15:16 +0530

    Time: 17 ms (68.68%), Space: 45.7 MB (66.79%) - LeetHub

---
+++ 0868-push-dominoes/0868-push-dominoes.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String pushDominoes(String dom) {
+        char ch[] = dom.toCharArray();
+        int n = ch.length;
+        int forces[] = new int[n];
+        int force = 0;
+        for(int i = 0 ;i < n; i++){
+            if(ch[i] == 'R'){
+                force = n;
+            }else if(ch[i] == 'L'){
+                force = 0;
+            }else{
+                force = Math.max(force-1, 0);
+            }
+            forces[i] = force;
+        }
+        force = 0;
+        for(int i = n-1; i >= 0; i--){
+            if(ch[i] == 'L'){
+                force = n;
+            }else if(ch[i] == 'R'){
+                force = 0;
+            }else{
+                force = Math.max(force-1, 0);
+            }
+            forces[i] -= force;
+        }
+        char ans[] = new char[n];
+        for(int i = 0; i < n; i++){
+            if(forces[i] > 0){
+                ans[i] = 'R';
+            }else if(forces[i] < 0){
+                ans[i] = 'L';
+            }else{
+                ans[i] = '.';
+            }
+        }
+        return new String(ans);
+    }
+}
