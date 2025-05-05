8a406074e70f6ec167eee6e10842d5ca3e4ca166
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-05-05 06:08:18 +0530

    Time: 484 ms (91.9%), Space: 0B (100%) - LeetHub

--- 1523-capital-gainloss/1523-capital-gainloss.sql
+++ 1523-capital-gainloss/1523-capital-gainloss.sql
@@ -1,27 +1,10 @@
-with stocks_table_buy (stock_name, total_loss) AS
-    (SELECT 
-    s.stock_name,
-    SUM(price)
-    from
-    Stocks s
-    WHERE s.operation = 'Buy'
-    GROUP BY s.stock_name), 
-
-    stocks_table_sell (stock_name, total_profit) AS
-    (SELECT 
-    s.stock_name,
-    SUM(price)
-    from
-    Stocks s
-    WHERE s.operation = 'Sell'
-    GROUP BY s.stock_name)
-
-
 SELECT 
-stb.stock_name,
-(sts.total_profit - stb.total_loss) as capital_gain_loss
-FROM 
-stocks_table_buy stb
-JOIN 
-stocks_table_sell sts
-ON stb.stock_name = sts.stock_name;
+    stock_name,
+    SUM(
+        CASE
+            WHEN operation = 'buy' THEN -price
+            WHEN operation = 'sell' THEN price
+            END
+    ) AS capital_gain_loss
+FROM Stocks
+GROUP BY stock_name;
