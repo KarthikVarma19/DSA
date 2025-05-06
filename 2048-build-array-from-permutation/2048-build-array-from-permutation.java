253d72d150b0d15de939d5dc6a3752ce57e4e651
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-05-06 17:25:16 +0530

    Time: 2 ms (7.3%), Space: 45.5 MB (27.48%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.java
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.java
@@ -1,10 +1,15 @@
 class Solution {
     public int[] buildArray(int[] nums) {
         int n = nums.length;
-        int ans[] = new int[n];
         for(int i = 0; i < n; i++){
-            ans[i] = nums[nums[i]];
+            // doing % mod since somebody might modified it 
+            int modified = 1000 * (nums[nums[i]] % 1000);
+            // previous value plus our 1000 based original value
+            nums[i] += modified;
         }
-        return ans;
+        for(int i = 0; i < n; i++){
+            nums[i] = nums[i]/1000;
+        }
+        return nums;
     }
 }
