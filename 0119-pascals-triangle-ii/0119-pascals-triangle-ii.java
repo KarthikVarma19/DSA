f24295ffd006b5dccf6a5c8e836b88af749bd992
Karthik Varma Ganaraju <ganarajukarthikvarma1635@gmail.com>
2025-05-15 16:40:55 +0530

    Time: 0 ms (100%), Space: 41.2 MB (48.54%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -1,17 +1,13 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
         List<Integer> ans = new ArrayList<>();
-        for(int i = 0; i <= rowIndex; i++){
-            ans.add(nCr(rowIndex, i));
-        }
-        return ans;
-    }
-    private int nCr(int n, int r){
         long res = 1;
-        for(int i = 0; i < r; i++){
-            res *= (n-i);
+        for(int i = 0; i < rowIndex; i++){
+            ans.add((int)res);
+            res *= (rowIndex-i);
             res /= (i+1);
         }
-        return (int)res;
+        ans.add(1);
+        return ans;
     }
 }
